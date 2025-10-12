8f077e2a320b916c0139ec09a91d07f9d95eb3a2
Ahmed <ahmed.phpdeveloper@gmail.com>
2025-10-12 21:06:17 +0500

    AI Search Tool

--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -6,6 +6,7 @@ import { paginationOptsValidator } from "convex/server";
 import { escalateConversation } from "../system/ai/tools/escalateConversation";
 import { resolveConversation } from "../system/ai/tools/resolveConversation";
 import { saveMessage } from "@convex-dev/agent";
+import { search } from "../system/ai/tools/search";
 
 export const create = action({
     args: {
@@ -55,14 +56,13 @@ export const create = action({
         if(shouldTriggerAgent) {
             await supportAgent.generateText(
                 ctx,
-                {
-                    threadId: args.threadId   
-                },
+                { threadId: args.threadId },
                 {
                     prompt: args.prompt,
                     tools: { 
-                        escalateConversation, 
-                        resolveConversation
+                        escalateConversationTool: escalateConversation, 
+                        resolveConversationTool: resolveConversation,
+                        searchTool: search
                     },
                 }
             )
